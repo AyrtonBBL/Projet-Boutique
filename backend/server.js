3637516c02f2434eaf374e7a95b07ba3c3e08959
AyrtonBLD <ayrton.blondeau@ynov.com>
2026-04-28 12:08:05 +0200

    changement server.js et ajout de images pour inserer les 25 et les faires dans product

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,20 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 const apiRoutes = require('./routes/api');
 
 const app = express();
 const PORT = 3000;
 
-// Autorise Kyky à se connecter à l' API
 app.use(cors());
 
 app.use(express.json());
 
-// Utilise les routes définies dans api.js
+app.use('/images', express.static(path.join(__dirname, 'public/images')));
+
 app.use('/api', apiRoutes);
 
 app.listen(PORT, () => {
-    console.log(`Serveur lancé sur http://localhost:${PORT}`);
+    console.log(`Serveur Y-Shop lancé sur : http://localhost:${PORT}`);
+    console.log(`les images sont accessibles ici : http://localhost:3000/images/nom_de_ton_image.png`);
 });
